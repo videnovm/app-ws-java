996ffc21988b998b60b2916b999297d40a520daf
Milos Videnov <milos.videnov@gmail.com>
2020-11-23 19:46:43 +0100

    Upload initial project repository

---
+++ src/main/java/com/app/ws/service/UserService.java
@@ -0,0 +1,7 @@
+package com.app.ws.service;
+
+import com.app.ws.shared.dto.UserDto;
+
+public interface UserService {
+	UserDto createUser(UserDto user);
+} 
---
+++ src/main/java/com/app/ws/ui/controller/UserController.java
@@ -0,0 +1,55 @@
+package com.app.ws.ui.controller;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.ws.service.UserService;
+import com.app.ws.shared.dto.UserDto;
+import com.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.app.ws.ui.model.response.UserRest;
+
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
+
+@RestController
+@RequestMapping("users") //http://localhost:8080/users
+public class UserController {
+	
+	@Autowired
+	UserService userService;
+	
+	@GetMapping
+	public String getUser() {
+		return "get user was called";
+	}
+	
+	@PostMapping
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		UserDto userDto = new UserDto();
+		
+		BeanUtils.copyProperties(userDetails, userDto);
+		
+		UserDto createdUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createdUser, returnValue);
+		
+		
+		return returnValue;
+	}
+	
+	@PutMapping
+	public String updateUser() {
+		return "update user was called";
+	}
+	
+	@DeleteMapping
+	public String deleteUser() {
+		return "delete user was called";
+	}
+}
