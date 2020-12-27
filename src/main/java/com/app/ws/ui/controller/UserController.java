2fb737a1e763c90ad42eee5dfd304c39493093c8
Milos Videnov <milos.videnov@gmail.com>
2020-12-27 19:14:34 +0200

    JSON & XML Support

--- src/main/java/com/app/ws/ui/controller/UserController.java
+++ src/main/java/com/app/ws/ui/controller/UserController.java
@@ -2,6 +2,7 @@ package com.app.ws.ui.controller;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,40 +20,39 @@ import com.app.ws.ui.model.response.UserRest;
 import ch.qos.logback.core.joran.util.beans.BeanUtil;
 
 @RestController
-@RequestMapping("users") //http://localhost:8080/users
+@RequestMapping("users") // http://localhost:8080/users
 public class UserController {
-	
+
 	@Autowired
 	UserService userService;
-	
-	@GetMapping(path="/{id}")
+
+	@GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public UserRest getUser(@PathVariable String id) {
-		UserRest returnValue = new UserRest(); 
-		
+		UserRest returnValue = new UserRest();
+
 		UserDto userDto = userService.getUserByUserId(id);
 		BeanUtils.copyProperties(userDto, returnValue);
 		return returnValue;
 	}
-	
-	@PostMapping
+
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
 		UserRest returnValue = new UserRest();
 		UserDto userDto = new UserDto();
-		
+
 		BeanUtils.copyProperties(userDetails, userDto);
-		
+
 		UserDto createdUser = userService.createUser(userDto);
 		BeanUtils.copyProperties(createdUser, returnValue);
-		
-		
+
 		return returnValue;
 	}
-	
+
 	@PutMapping
 	public String updateUser() {
 		return "update user was called";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
 		return "delete user was called";
