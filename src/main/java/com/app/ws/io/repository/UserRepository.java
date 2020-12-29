39e83b49452108aba96d47cb7a3627b7d4db1e8d
Milos Videnov <milos.videnov@gmail.com>
2020-12-29 20:49:10 +0200

    Get all users from database and Pagination

--- src/main/java/com/app/ws/io/repository/UserRepository.java
+++ src/main/java/com/app/ws/io/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package com.app.ws.io.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.app.ws.io.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
 	UserEntity findUserByEmail(String email);
 	UserEntity findByUserId(String userId);
 	//UserEntity findByLastName(String lastName);
